e4b29bf64db3fdc83a37930d76e9ef4fe2018ef2
grzesiek4211 <grzesiek4211@gmail.com>
2025-06-29 20:50:31 +0200

    Removing old screenshots

--- app/src/main/java/com/example/myapplication/presentation/StopFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/StopFragment.kt
@@ -2,12 +2,16 @@ package com.example.myapplication.presentation
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.DATE_ADDED
+import android.provider.MediaStore.Images.Media.RELATIVE_PATH
+import android.provider.MediaStore.Images.Media._ID
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +24,9 @@ import com.example.myapplication.R
 import com.example.myapplication.presentation.MainActivity.Companion.ONGOING_NOTIFICATION_ID
 import com.example.myapplication.presentation.TimerFragment.Companion.TIMER_REQUEST_CODE
 import java.time.LocalDateTime
+
+private const val SCREENSHOTS_LOCATION = "Pictures/Football_Referee"
+private const val WEEK_IN_SECS = 7 * 24 * 60 * 60L
 
 class StopFragment : Fragment() {
 
@@ -63,6 +70,7 @@ class StopFragment : Fragment() {
             val notificationManager =
                 requireContext().getSystemService(Context.NOTIFICATION_SERVICE) as android.app.NotificationManager
             notificationManager.cancel(ONGOING_NOTIFICATION_ID)
+            removeScreenshotsOlderThan(WEEK_IN_SECS)
             saveScreenshotToMediaStore(sharedScreenshotViewModel.bitmap.value!!)
             restartApp()
         }
@@ -87,9 +95,9 @@ class StopFragment : Fragment() {
     private fun saveScreenshotToMediaStore(bitmap: Bitmap) {
         try {
             val contentValues = ContentValues().apply {
-                put(MediaStore.Images.Media.DISPLAY_NAME, "${LocalDateTime.now()}_screenshot_summary.png")
+                put(MediaStore.Images.Media.DISPLAY_NAME, "${LocalDateTime.now()}_summary.png")
                 put(MediaStore.Images.Media.MIME_TYPE, "image/png")
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/Screenshots")
+                put(RELATIVE_PATH, SCREENSHOTS_LOCATION)
             }
 
             val resolver = context?.contentResolver
@@ -104,6 +112,36 @@ class StopFragment : Fragment() {
             }
         } catch (e: Exception) {
             Log.e("ScreenshotError", "Failed to save screenshot: ${e.message}", e)
+        }
+    }
+
+    private fun removeScreenshotsOlderThan(secs: Long) {
+        val resolver = context?.contentResolver ?: return
+
+        val oneWeekAgoInSeconds = System.currentTimeMillis() / 1000 - secs
+
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(_ID, DATE_ADDED, RELATIVE_PATH)
+
+        val selection = "$RELATIVE_PATH LIKE ? AND $DATE_ADDED < ?"
+        val selectionArgs = arrayOf("%$SCREENSHOTS_LOCATION%", oneWeekAgoInSeconds.toString())
+        val cursor = resolver.query(uri, projection, selection, selectionArgs, null)
+
+        cursor?.use {
+            val idColumn = it.getColumnIndexOrThrow(_ID)
+            while (it.moveToNext()) {
+                val id = it.getLong(idColumn)
+                val imageUri = ContentUris.withAppendedId(uri, id)
+
+                try {
+                    val rows = resolver.delete(imageUri, null, null)
+                    Log.d("ScreenshotCleanup", "Deleted URI: $imageUri (rows: $rows)")
+                } catch (e: SecurityException) {
+                    Log.e("ScreenshotCleanup", "SecurityException deleting $imageUri", e)
+                } catch (e: Exception) {
+                    Log.e("ScreenshotCleanup", "Error deleting $imageUri", e)
+                }
+            }
         }
     }
 
